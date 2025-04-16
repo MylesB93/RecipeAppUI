13bd0ca15b8826af813f5d0ec673200c5ad9325f
Myles Broomes <mylesbroomes@hotmail.co.uk>
2025-04-16 22:39:24 +0100

    Remove bin and obj folders from source control and add core class library

---
+++ RecipeAppUI.Core/Platforms/MacCatalyst/PlatformClass1.cs
@@ -0,0 +1,7 @@
+﻿namespace RecipeAppUI.Core
+{
+	// All the code in this file is only included on Mac Catalyst.
+	public class PlatformClass1
+	{
+	}
+}
---
+++ RecipeAppUI.Core/Platforms/iOS/PlatformClass1.cs
@@ -0,0 +1,7 @@
+﻿namespace RecipeAppUI.Core
+{
+	// All the code in this file is only included on iOS.
+	public class PlatformClass1
+	{
+	}
+}
