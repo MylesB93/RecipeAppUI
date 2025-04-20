077ce90cb026d6ed20766cfc67b7009294f693fc
Myles Broomes <mylesbroomes@hotmail.co.uk>
2025-04-20 23:30:13 +0100

    Add recipe view model to display recipes

--- RecipeAppUI/MainPage.xaml.cs
+++ RecipeAppUI/MainPage.xaml.cs
@@ -4,27 +4,13 @@ namespace RecipeAppUI
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
         private readonly IRecipeService _recipeService;
 
         public MainPage(IRecipeService recipeService)
         {
-            InitializeComponent();
-            _recipeService = recipeService;
-        }
-
-        private async void OnCounterClicked(object sender, EventArgs e)
-        {
-            var test = await _recipeService.GetAllRecipesAsync();
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
-        }
+			InitializeComponent();
+			BindingContext = new MainViewModel(recipeService);
+		}
     }
 
 }
---
+++ RecipeAppUI/ViewModels/RecipeViewModel.cs
@@ -0,0 +1,32 @@
+﻿using RecipeAppUI.Core.Interfaces;
+using RecipeAppUI.Core.Models;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+public class MainViewModel : INotifyPropertyChanged
+{
+	private readonly IRecipeService _recipeService;
+
+	public ObservableCollection<Recipe> Recipes { get; set; } = new();
+
+	public MainViewModel(IRecipeService recipeService)
+	{
+		_recipeService = recipeService;
+		LoadRecipes();
+	}
+
+	private async void LoadRecipes()
+	{
+		var recipes = await _recipeService.GetAllRecipesAsync();
+		foreach (var recipe in recipes)
+		{
+			Recipes.Add(recipe);
+		}
+	}
+
+	public event PropertyChangedEventHandler PropertyChanged;
+
+	protected void OnPropertyChanged([CallerMemberName] string name = null) =>
+		PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+}
