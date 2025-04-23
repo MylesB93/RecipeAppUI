e2ff951c7cb1d9e19c74c5b4142280e72e62c10f
Myles Broomes <mylesbroomes@hotmail.co.uk>
2025-04-23 22:08:32 +0100

    Add web app url as API base url

--- RecipeAppUI/MauiProgram.cs
+++ RecipeAppUI/MauiProgram.cs
@@ -1,33 +1,41 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using RecipeAppUI.Core.Interfaces;
 using RecipeAppUI.Core.Services;
 
 namespace RecipeAppUI
 {
-    public static class MauiProgram
-    {
-        public static MauiApp CreateMauiApp()
-        {
-            var builder = MauiApp.CreateBuilder();
-            builder
-                .UseMauiApp<App>()
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
+	public static class MauiProgram
+	{
+		public static MauiApp CreateMauiApp()
+		{
+			var builder = MauiApp.CreateBuilder();
+
+			var config = new ConfigurationBuilder()
+							.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+							.Build();
+
+			var baseUrl = config["RecipeAPI:BaseUrl"] ?? throw new Exception("Base URL is missing from configuration.");
+
+			builder
+				.UseMauiApp<App>()
+				.ConfigureFonts(fonts =>
+				{
+					fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+					fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+				});
 
 #if DEBUG
-    		builder.Logging.AddDebug();
+			builder.Logging.AddDebug();
 #endif
 			builder.Services.AddSingleton(new HttpClient
 			{
-				BaseAddress = new Uri("https://localhost:44383/")
+				BaseAddress = new Uri(baseUrl)
 			});
 
 			builder.Services.AddSingleton<IRecipeService, RecipeService>();
 
 			return builder.Build();
-        }
-    }
+		}
+	}
 }
