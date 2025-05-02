0ab1a52f60338e4034cfcc9752c3d7cc9931a678
Myles Broomes <mylesbroomes@hotmail.co.uk>
2025-05-02 21:57:32 +0100

    Ensure properties are nullable

--- RecipeAppUI.Core/Models/Response.cs
+++ RecipeAppUI.Core/Models/Response.cs
@@ -4,37 +4,37 @@ namespace RecipeAppUI.Core.Models
 {
 	public class Rootobject
 	{
-		public Item[] Items { get; set; }
+		public Item[]? Items { get; set; }
 	}
 
 	public class Item
 	{
-		public string Name { get; set; }
-		public Properties Properties { get; set; }
-		public string Id { get; set; }
+		public string? Name { get; set; }
+		public Properties? Properties { get; set; }
+		public string? Id { get; set; }
 	}
 
 	public class Properties
 	{
-		public Recipeingredient[] RecipeIngredients { get; set; }
-		public RecipeUtensil[] RecipeUtensils { get; set; }
-		public Cookinginstructions CookingInstructions { get; set; }
+		public Recipeingredient[]? RecipeIngredients { get; set; }
+		public RecipeUtensil[]? RecipeUtensils { get; set; }
+		public Cookinginstructions? CookingInstructions { get; set; }
 	}
 
 	public class Recipeingredient
 	{
-		public string Name { get; set; }
-		public Properties1 Properties { get; set; }
+		public string? Name { get; set; }
+		public Properties1? Properties { get; set; }
 	}
 
 	public class RecipeUtensil
 	{
-		public string Name { get; set; }
+		public string? Name { get; set; }
 	}
 
 	public class Cookinginstructions
 	{
-		public string markup { get; set; }
+		public string? Markup { get; set; }
 	}
 
 	public class Properties1
