aa173f9f66285c2d730f0612b642e71f10c4319b
Myles Broomes <mylesbroomes@hotmail.co.uk>
2025-06-17 21:55:00 +0100

    Add search functionality

--- RecipeAppUI/ViewModels/RecipesViewModel.cs
+++ RecipeAppUI/ViewModels/RecipesViewModel.cs
@@ -3,12 +3,29 @@ using RecipeAppUI.Core.Models;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Linq;
 
 public class RecipesViewModel : INotifyPropertyChanged
 {
 	private readonly IRecipeService _recipeService;
 
 	public ObservableCollection<Recipe> Recipes { get; set; } = new();
+	public ObservableCollection<Recipe> FilteredRecipes { get; set; } = new();
+
+	private string _searchText = string.Empty;
+	public string SearchText
+	{
+		get => _searchText;
+		set
+		{
+			if (_searchText != value)
+			{
+				_searchText = value;
+				OnPropertyChanged();
+				FilterRecipes();
+			}
+		}
+	}
 
 	public RecipesViewModel(IRecipeService recipeService)
 	{
@@ -19,9 +36,23 @@ public class RecipesViewModel : INotifyPropertyChanged
 	private async void LoadRecipes()
 	{
 		var recipes = await _recipeService.GetAllRecipesAsync();
+		Recipes.Clear();
 		foreach (var recipe in recipes)
 		{
 			Recipes.Add(recipe);
+		}
+		FilterRecipes();
+	}
+
+	private void FilterRecipes()
+	{
+		FilteredRecipes.Clear();
+		var filtered = string.IsNullOrWhiteSpace(SearchText)
+			? Recipes
+			: Recipes.Where(r => r.Name.Contains(SearchText, StringComparison.OrdinalIgnoreCase));
+		foreach (var recipe in filtered)
+		{
+			FilteredRecipes.Add(recipe);
 		}
 	}
 
